44e8a25bf85867d1a27e938fb0408bff9e1a670a
peacebefore <peacebefore@gmail.com>
2023-10-09 16:07:34 -0700

    basic converter complete

--- main.py
+++ main.py
@@ -14,4 +14,5 @@ if not os.path.exists(output_folder):
 
 for filename in os.listdir(image_folder):
     img = Image.open(f'{image_folder}{filename}')
-    img.save(f'{output_folder}{filename}.png', 'png')
+    clean_name = os.path.splitext(filename)[0]
+    img.save(f'{output_folder}{clean_name}.png', 'png')
