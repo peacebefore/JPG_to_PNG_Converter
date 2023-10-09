4ac41bc21194dee6a5985c2183d018e5eeb9ac1f
peacebefore <peacebefore@gmail.com>
2023-10-09 15:57:08 -0700

    initial commit for JPG to PNG Converter

---
+++ main.py
@@ -0,0 +1,17 @@
+import sys
+import os
+from PIL import Image
+
+image_folder = sys.argv[1]
+output_folder = sys.argv[2]
+
+# grab first and second argument
+print(image_folder, output_folder)
+
+# check if new folder exists, if not create it
+if not os.path.exists(output_folder):
+    os.makedirs(output_folder)
+
+for filename in os.listdir(image_folder):
+    img = Image.open(f'{image_folder}{filename}')
+    img.save(f'{output_folder}{filename}.png', 'png')
